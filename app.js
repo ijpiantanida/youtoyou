59cd29d5863699f6f9693bc6cd0ad1dc2be938a5
Ignacio Piantanida <ijpiantanida@gmail.com>
2015-03-23 00:58:19 -0300

    Add self ping

--- app.js
+++ app.js
@@ -2,30 +2,41 @@ var express = require('express')
 var ffmpeg = require('fluent-ffmpeg')
 var app = express()
 
+require('heroku-self-ping')(process.env.APP_URL)
+
 app.get('/', function (req, res) {
   var ytdl = require('ytdl-core')
 
-  var url = req.query.url || 'http://www.youtube.com/watch?v=A02s8omM_hI'
+  var url = req.query.url
 
-  var stream = ytdl(url, {filter: "audioonly"})
+  if(url == undefined){
+    res.send('Hello World!')
+  }else{
+    var stream = ytdl(url, {filter: "audioonly"})
 
-  console.info("Starting processing on \"" + url + "\"")
-  stream.on("info",function(info, format){
-    console.info("Finished fetching info - \"" + info.title + "\"")
-    res.writeHead(200, {
-      'Content-Type': 'audio/mpeg',
-      'Content-Lenght': format.size,
-      'X-Content-Duration': info.length_seconds,
-      'Content-disposition': 'attachment; filename="' + info.title + '.mp3"'
-    });
-    ffmpeg(stream)
-      .toFormat('mp3')
-      .on('end', function() {
-        console.log('Processing finished!');
+    console.info("Starting processing on \"" + url + "\"")
+    stream.on("info",function(info, format){
+      console.info("Finished fetching info - \"" + info.title + "\"")
+      res.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+        'Content-Lenght': format.size,
+        'X-Content-Duration': info.length_seconds,
+        'Content-disposition': 'attachment; filename="' + info.title + '.mp3"'
       })
-      .pipe(res)
-  });
+
+      ffmpeg(stream)
+        .toFormat('mp3')
+        .on('end', function() {
+          console.log('Processing finished!');
+        })
+        .on('error', function(){
+          res.send('ERROR!')
+        })
+        .pipe(res)
+    })
+  }
 })
+
 
 var port = process.env.PORT || 3000
 var server = app.listen(port, function () {
