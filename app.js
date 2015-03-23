c5a4eec5e6fb6ff23efd3480b458106d8b558d00
Ignacio Piantanida <ijpiantanida@gmail.com>
2015-03-22 22:05:58 -0300

    Return audio stream from video

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  var ytdl = require('ytdl-core');
+
+  var url = req.query.url || 'http://www.youtube.com/watch?v=A02s8omM_hI';
+
+  var stream = ytdl(url, {filter: "audioonly"});
+  stream.on("info",function(info, format){ 
+    res.writeHead(200, {
+      'Content-Type': 'audio/mpeg',
+      'Content-Lenght': format.size,
+      'X-Content-Duration': info.length_seconds,
+      'Content-disposition': 'attachment; filename="' + info.description + '"'
+    });
+    stream.pipe(res);
+  });
+})
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Youtoyou listening at http://%s:%s', host, port);
+})
