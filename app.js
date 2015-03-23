f096f6e7afefb0d4cdfae8bef2fbb7ea805633f7
Ignacio Piantanida <ijpiantanida@gmail.com>
2015-03-22 23:44:04 -0300

    Convert file to mp3

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 var express = require('express')
+var ffmpeg = require('fluent-ffmpeg')
 var app = express()
 
 app.get('/', function (req, res) {
-  var ytdl = require('ytdl-core');
+  var ytdl = require('ytdl-core')
 
-  var url = req.query.url || 'http://www.youtube.com/watch?v=A02s8omM_hI';
+  var url = req.query.url || 'http://www.youtube.com/watch?v=A02s8omM_hI'
 
-  var stream = ytdl(url, {filter: "audioonly"});
-  stream.on("info",function(info, format){ 
+  var stream = ytdl(url, {filter: "audioonly"})
+
+  console.info("Starting processing on \"" + url + "\"")
+  stream.on("info",function(info, format){
+    console.info("Finished fetching info - \"" + info.title + "\"")
     res.writeHead(200, {
       'Content-Type': 'audio/mpeg',
       'Content-Lenght': format.size,
       'X-Content-Duration': info.length_seconds,
-      'Content-disposition': 'attachment; filename="' + info.description + '"'
+      'Content-disposition': 'attachment; filename="' + info.title + '.mp3"'
     });
-    stream.pipe(res);
+    ffmpeg(stream)
+      .toFormat('mp3')
+      .on('end', function() {
+        console.log('Processing finished!');
+      })
+      .pipe(res)
   });
 })
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000
 var server = app.listen(port, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
+  var host = server.address().address
+  var port = server.address().port
 
-  console.log('Youtoyou listening at http://%s:%s', host, port);
+  console.log('Youtoyou listening at http://%s:%s', host, port)
 })
